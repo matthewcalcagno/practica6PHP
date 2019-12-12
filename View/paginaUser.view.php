0b282c4a12d459af99799f2a41286ae0133581d0
m.aedo@sapalomera.cat <56046307+matias-aedo-soto@users.noreply.github.com>
2019-12-12 16:33:54 +0100

    acabar

--- View/paginaUser.view.php
+++ View/paginaUser.view.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
 //Matias Aedo
     // perfil d'usuari que es mostrara sempre i quan la sessio esta activa
     // rescata les dades de la cookie
     echo "<h1>Perfil d'usuari</h1>";
     echo "Usuari; ".$_SESSION['username']."<br>";
-    echo "Data; ".$_COOKIE["data"]."<br>";
+    echo "Data; "."<br>";
     
 ?>
