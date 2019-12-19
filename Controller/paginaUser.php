e519d1877f51114ec9f0c9884334fa2c16a96bc0
m.aedo@sapalomera.cat <56046307+matias-aedo-soto@users.noreply.github.com>
2019-12-19 17:53:48 +0100

    Todo listo funcionando

--- Controller/paginaUser.php
+++ Controller/paginaUser.php
@@ -1,8 +1,9 @@
 <?php 
 session_start();
 /*
-  Agafa desde el servidor les dades del usuari, permeteix que l'usuari pugi cambiar el seu fons.
-*/
+  Agafa desde el servidor les dades del usuari, permeteix que l'usuari pugi cambiar el seu fons, es guardara l'ultim fons.
+  La nova imatge es carrega quan l'usuari recarrega la pagina.
+  */
 //intenta conectarse a la base de dades si la troba
 try {
 	$connexio = new PDO('mysql:host=localhost;dbname=practica6', 'root', '');
@@ -17,6 +18,16 @@ try {
   $resultats->execute();
   $user = $resultats->fetch(PDO::FETCH_ASSOC);
   $idUser = (int)$user['id'];
+
+
+  //sentencia que comprova si hi ha algun background disponible per l'usuari
+  $resultats = $connexio->prepare("SELECT link_bg FROM background_user WHERE id_user = :idUser ORDER BY id_bg DESC LIMIT 1");
+  $resultats->bindValue(":idUser", $idUser,PDO::PARAM_INT) ;
+  $resultats->execute();
+  $back = $resultats->fetch(PDO::FETCH_ASSOC);
+  $link_bg = $back['link_bg'];
+  echo $link_bg;
+
   //si es fa un POST agafara el link i el pujara a la base de dades
   if($_SERVER["REQUEST_METHOD"] == "POST"){
     // introduim el link a la seva taula corresponent
@@ -26,13 +37,16 @@ try {
     $resultats->bindValue(":link", $imatgeDFons,PDO::PARAM_STR) ;
     $resultats->execute();
     //fem una consulta per saber el id del background que hem afegit
-    $resultats = $connexio->prepare("SELECT id_bg FROM background_user WHERE id_user = :idUser");
+    $resultats = $connexio->prepare("SELECT id_bg FROM background_user WHERE id_user = :idUser ORDER BY id_bg DESC LIMIT 1");
     $resultats->bindValue(":idUser", $idUser,PDO::PARAM_INT) ;
+    $resultats->execute();
     $id_bg = $resultats->fetch(PDO::FETCH_ASSOC);
     //introduim l'id de background al usuari
     $idBg = (int)$id_bg['id_bg'];
-    $resultats = $connexio->prepare("UPDATE TABLE SET id_background = :id_bg");
-    $resultats->bindValue(":id_bg", $idBg,PDO::PARAM_INT);
+    // actualizem la taula users per afegirli l'id corresponent 
+    $resultats = $connexio->prepare("UPDATE users SET id_background = :id_bg WHERE id = :idUser");
+    $resultats->bindValue(":id_bg", $idBg);
+    $resultats->bindValue(":idUser", $idUser) ;
     $resultats->execute();
   }
 ?>
--- View/paginaUser.view.php
+++ View/paginaUser.view.php
@@ -1,3 +1,8 @@
+<?php
+    // Pagina view del usuari, on cridarem al controlador perquer agafi les dades de la BDD
+    // Cridem l'arxiu que conectara amb el servidor
+    require_once "../Controller/paginaUser.php";
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,20 +10,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style type="text/css">
+        body {
+            background-image: url(<?php echo $link_bg?>);
+        }
+    </style>
 </head>
 <body>
-<?php
-    //Matias Aedo
-    // Cridem l'arxiu que conectara amb el servidor
-    require_once "../Controller/paginaUser.php";
-    ?>
-</body>
-</html>
     <?php echo "<h1>Perfil d'usuari</h1>"; ?>
     <?php echo "Id; ".$user['id']."<br>"; ?>
     <?php echo "Usuari; ".$user['user']."<br>";?>
     <form <?php echo "../Controller/paginaUser.php"; ?> method="post">
-    Select image to upload:
-    <input type="text" name="imatge" id="imatge">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
+        Digues el link de la imatge:
+        <input type="text" name="imatge" id="imatge">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+</body>
+</html>
+    
